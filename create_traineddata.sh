2459739223bcd303284fc19d1d693fd711a48fb2
Your Name <you@example.com>
2022-08-03 22:01:23 -0700

    Added code to replace eng.word-dawg; default word list from Tesseract

--- create_traineddata.sh
+++ create_traineddata.sh
@@ -7,5 +7,15 @@ combine_tessdata -e eng.traineddata eng.unicharset
 ## Create eng.punc-dawg using new character list
 wordlist2dawg eng.punc eng.punc-dawg eng.unicharset
 
+## Create eng.word-dawg using new character list
+wordlist2dawg eng.wordlist eng.word-dawg eng.unicharset
+
 ## Replace eng.punc-dawg in eng.traineddata file
-combine_tessdata -o eng.traineddata eng.punc-dawg
+combine_tessdata -o eng.traineddata eng.punc-dawg eng.word-dawg
+
+## Delete intermediate files
+rm -f eng.word-dawg
+rm -f eng.traineddata.__tmp__
+
+## Create .gz version (used for Tesseract.js)
+gzip -kf eng.traineddata
